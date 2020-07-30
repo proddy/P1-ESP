ad5274994c777622bba36923307c68073fcdee08
Paul <paul@derbyshire.nl>
2020-07-30 12:47:06 +0200

    initial checkin

---
+++ src/p1gateway.h
@@ -0,0 +1,70 @@
+-#pragma once
+
+#include <FS.h>
+#include <EEPROM.h>
+#include <DNSServer.h>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <WiFiManager.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
+#include <SoftwareSerial.h>
+
+// * Baud rate for both hardware and software serial
+#define BAUD_RATE 115200
+
+// * P1 Meter RX pin
+#define P1_SERIAL_RX D2
+// #define P1_SERIAL_RX 3
+
+// * Max telegram length
+#define P1_MAXLINELENGTH 64
+
+// * The hostname of our little creature
+#define HOSTNAME "p1meter"
+
+// * The password used for OTA
+#define OTA_PASSWORD "admin"
+
+// * Wifi timeout in milliseconds
+#define WIFI_TIMEOUT 30000
+
+// * MQTT network settings
+#define MQTT_MAX_RECONNECT_TRIES 10
+
+// * MQTT root topic
+#define MQTT_ROOT_TOPIC "sensors/power/p1meter"
+
+// * MQTT Last reconnection counter
+long LAST_RECONNECT_ATTEMPT = 0;
+
+// * To be filled with EEPROM data
+char MQTT_HOST[64] = "";
+char MQTT_PORT[6]  = "";
+char MQTT_USER[32] = "";
+char MQTT_PASS[32] = "";
+
+// * Set to store received telegram
+char telegram[P1_MAXLINELENGTH];
+
+// * Set to store the data values read
+long CONSUMPTION_LOW_TARIF;
+long CONSUMPTION_HIGH_TARIF;
+long ACTUAL_CONSUMPTION;
+long INSTANT_POWER_CURRENT;
+long INSTANT_POWER_USAGE;
+long GAS_METER_M3;
+
+// Set to store data counters read
+long ACTUAL_TARIF;
+long SHORT_POWER_OUTAGES;
+long LONG_POWER_OUTAGES;
+long SHORT_POWER_DROPS;
+long SHORT_POWER_PEAKS;
+
+// * Set during CRC checking
+unsigned int currentCRC = 0;
+
+void tick();
